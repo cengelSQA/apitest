403d0d00ce63cff3de40d1ea7f4f45ef90ee2ecf
Chase Engel <cengel@QAtestings-Mac-mini.local>
2016-06-24 16:27:04 -0700

    Added tests

--- test_api.rb
+++ test_api.rb
@@ -1,7 +1,17 @@
 require 'minitest/autorun'
 require 'rest-client'
+require 'json'
 
 class TestApi < Minitest::Test
-  def test_api
+  def setup
+    @response = RestClient.get 'http://localhost:4567/'
+  end
+
+  def test_code
+    assert_equal @response.code, 200
+  end
+
+  def test_message
+    assert_equal JSON.parse(@response)['message'], 'hello'
   end
 end
